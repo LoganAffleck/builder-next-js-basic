8676dca82685592d14f937ccf2458e2086017801
J. Logan Affleck <affleck.logan@gmail.com>
2022-10-10 19:00:39 -0400

    Modified font import

--- pages/[[...page]].tsx
+++ pages/[[...page]].tsx
@@ -57,7 +57,7 @@ export default function Page({
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet"></link>
         {!page && <meta name="robots" content="noindex" />}
       </Head>
