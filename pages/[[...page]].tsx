5451019b210070bf0042f99a50a8080991b61681
J. Logan Affleck <affleck.logan@gmail.com>
2022-10-10 18:59:33 -0400

    Add Poppins

--- pages/[[...page]].tsx
+++ pages/[[...page]].tsx
@@ -56,6 +56,9 @@ export default function Page({
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet"></link>
         {!page && <meta name="robots" content="noindex" />}
       </Head>
       {show404 ? (
